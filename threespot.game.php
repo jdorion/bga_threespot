e46580120812ddf3aefa46e67b1074484452ae56
J.T. Dorion <jdorion@gmail.com>
2021-02-28 16:15:52 -0500

    code rev 3 - little bug with cards but just js

--- threespot.game.php
+++ threespot.game.php
@@ -78,6 +78,36 @@ class ThreeSpot extends Table
         self::reloadPlayersBasicInfos();
         
         /************ Start the game initialization *****/
+        // Init global values with their initial values
+
+        // Note: hand types: 0 = give 3 cards to player on the left
+        //                   1 = give 3 cards to player on the right
+        //                   2 = give 3 cards to player opposite
+        //                   3 = keep cards
+        self::setGameStateInitialValue( 'currentHandType', 0 );
+        
+        // Set current trick color to zero (= no trick color)
+        self::setGameStateInitialValue( 'trickColor', 0 );
+        
+        // Create cards
+        $cards = array ();
+        foreach ( $this->colors as $color_id => $color ) {
+            // spade, heart, diamond, club
+            for ($value = 7; $value <= 14; $value ++) {
+                //  7, 3, 4, ... K, A
+                $cards [] = array ('type' => $color_id,'type_arg' => $value,'nbr' => 1 );
+            }
+        }
+        
+        $this->cards->createCards( $cards, 'deck' );
+
+        // Shuffle deck
+        $this->cards->shuffle('deck');
+        // Deal 8 cards to each player
+        $players = self::loadPlayersBasicInfos();
+        foreach ( $players as $player_id => $player ) {
+            $cards = $this->cards->pickCards(8, 'deck', $player_id);
+        } 
 
         // Init global values with their initial values
         //self::setGameStateInitialValue( 'my_first_global_variable', 0 );
@@ -117,7 +147,11 @@ class ThreeSpot extends Table
         $result['players'] = self::getCollectionFromDb( $sql );
   
         // TODO: Gather all information about current game situation (visible by player $current_player_id).
-  
+        // Cards in player hand
+        $result['hand'] = $this->cards->getCardsInLocation( 'hand', $current_player_id );
+                
+        // Cards played on the table
+        $result['cardsontable'] = $this->cards->getCardsInLocation( 'cardsontable' );
         return $result;
     }
 
